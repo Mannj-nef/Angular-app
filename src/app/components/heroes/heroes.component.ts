2b58ce8c8ff0a851adea8b22d2228544c763e3e6
Manh Quan <manhquan.05012002@gmail.com>
2023-07-10 00:30:18 +0700

    delete file test

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,27 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IHero } from './type';
 import { HeroService } from '../../services/hero.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent {
-  constructor(private heroService: HeroService) {}
+export class HeroesComponent implements OnInit {
   heroes: IHero[] = [];
 
   selectedHero?: IHero;
 
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessagesService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: IHero): void {
     this.selectedHero = hero;
+    this.messageService.add(
+      `HeroesComponent: Selected hero id=${hero.id}, name = ${hero.name}`
+    );
   }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
   }
 }
--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -4,8 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class MessagesService {
-  constructor() {}
-
   messages: string[] = [];
 
   add(message: string) {
